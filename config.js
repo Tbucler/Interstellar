d890b388d275e965796e24feb373c16742af8aed
Kayden <160945338+DarkTurns14@users.noreply.github.com>
2024-03-28 15:15:39 -0400

    Implement Password Protection + Revert calum's password back to old.

--- config.js
+++ config.js
@@ -1,10 +1,26 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: 'password',
+    Armaiez: "Chicken",
+    Calum: "Smile3828",
+    Dean: "AddictedToLegos",
+    Emma: "iloveketchup1526",
+    Emanuel: "24689",
+    Gabryl: "gabryl5872",
+    Giovanni: "124124",
+    Interstellar: "4523",
+    Jasmine: "IdkMisi",
+    Jayslee: "G3NSH1N_24",
+    JJ: "54455445",
+    Kale: "KaColon09",
+    Matthew: "Blood", // Matthew Hatch
+    Quentin: "Applesauce",
+    Xzavier: "Xzavier13",
+    Yeuri: "theonepieceisreal",
+    Zavian: "Bruvidk_4410",
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
-}
-export default config
+};
+export default config;
